bb742cec8444408ddc91401074c98cb3ac1b0fff
Shawn <codingshawn@gmail.com>
2021-05-13 19:56:47 +0800

    Implemented filtering of book list by genre

--- src/components/Books.js
+++ src/components/Books.js
@@ -1,11 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Books = (props) => {
+  const [selectedFilter, setSelectedFilter] = useState("");
+
   if (!props.show) {
     return null;
   }
 
   const books = props.books;
+
+  let genres = [];
+  books.map((book) =>
+    book.genres.map((genre) => (genres = genres.concat(genre)))
+  );
+  let uniqueGenres = [...new Set(genres)];
+
+  let filteredBooks = books.filter((book) =>
+    book.genres.find((genre) => genre === selectedFilter)
+  );
+
+  if (!selectedFilter) {
+    filteredBooks = books;
+  }
 
   return (
     <div>
@@ -18,7 +34,7 @@ const Books = (props) => {
             <th>author</th>
             <th>published</th>
           </tr>
-          {books.map((a) => (
+          {filteredBooks.map((a) => (
             <tr key={a.title}>
               <td>{a.title}</td>
               <td>{a.author.name}</td>
@@ -27,6 +43,12 @@ const Books = (props) => {
           ))}
         </tbody>
       </table>
+      {uniqueGenres.map((genre) => (
+        <button key={genre} onClick={() => setSelectedFilter(genre)}>
+          {genre}
+        </button>
+      ))}
+      <button onClick={() => setSelectedFilter("")}>All Genres</button>
     </div>
   );
 };

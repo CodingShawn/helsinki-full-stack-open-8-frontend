0a07a577f7f4b7f41e9ff079fa378168627d38a8
Shawn <codingshawn@gmail.com>
2021-05-13 18:23:57 +0800

    Added Logout functionality along with showing different user interface based on whether user is logged in or not

--- src/App.js
+++ src/App.js
@@ -2,16 +2,48 @@ import React, { useState } from "react";
 import Authors from "./components/Authors";
 import Books from "./components/Books";
 import NewBook from "./components/NewBook";
+import Login from "./components/Login";
 import { ALL_AUTHORS, ALL_BOOKS } from "./queries";
-import { useQuery } from "@apollo/client";
+import { useApolloClient, useQuery } from "@apollo/client";
 
 const App = () => {
   const [page, setPage] = useState("authors");
+  const [token, setToken] = useState(null);
+  const client = useApolloClient();
+
   const authorResults = useQuery(ALL_AUTHORS);
   const bookResults = useQuery(ALL_BOOKS);
 
+  function logout() {
+    setToken(null);
+    localStorage.clear();
+    client.resetStore();
+    setPage("login");
+  }
+
   if (authorResults.loading || bookResults.loading) {
     return <div>loading</div>;
+  }
+
+  if (!token) {
+    return (
+      <div>
+        <div>
+          <button onClick={() => setPage("authors")}>authors</button>
+          <button onClick={() => setPage("books")}>books</button>
+          <button onClick={() => setPage("login")}>Log in</button>
+        </div>
+
+        <Authors
+          authors={authorResults.data.allAuthors}
+          show={page === "authors"}
+        />
+
+        <Books books={bookResults.data.allBooks} show={page === "books"} />
+
+        {page === "login" && <Login setToken={setToken} />}
+      </div>
+    );
   }
 
   return (
@@ -20,6 +52,7 @@ const App = () => {
         <button onClick={() => setPage("authors")}>authors</button>
         <button onClick={() => setPage("books")}>books</button>
         <button onClick={() => setPage("add")}>add book</button>
+        <button onClick={logout}>Log out</button>
       </div>
 
       <Authors
